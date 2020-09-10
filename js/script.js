bea08f4d6d2e0c7ba263e5494b811baf1f8c103e
Valerii Karimov <valeraguess@gmail.com>
2020-09-10 23:27:12 +0300

    Sending data to server from contact forms

--- js/script.js
+++ js/script.js
@@ -128,7 +128,7 @@ document.addEventListener('keydown', (e) => {
   }
 })
 
-// const modalTimerId = setTimeout(openModal, 3000);
+const modalTimerId = setTimeout(openModal, 3000);
 
 function showModalByScroll() {
   if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
@@ -207,3 +207,56 @@ new MenuCard(
   16,
   '.menu .container'
 ).render();
+
+// Forms
+
+const forms = document.querySelectorAll('form');
+
+const message = {
+  loading: 'loading data',
+  success: 'thx, in future we conn with you',
+  failure: 'fail...'
+}
+
+forms.forEach(item => {
+  postData(item);
+});
+
+function postData(form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+    statusMessage.textContent = message.loading;
+    form.append(statusMessage);
+
+    const request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+
+    request.setRequestHeader('Content-type', 'application/json');
+    const formData = new FormData(form);
+
+    const object = {};
+    formData.forEach(function(value, key) {
+      object[key] = value;
+    });
+
+    const json = JSON.stringify(object);
+
+    request.send(json);
+
+    request.addEventListener('load', () => {
+        if(request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+    });
+  });
+}
