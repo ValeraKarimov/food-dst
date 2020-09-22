7be8ced4b1cf1dfc45a0221379eb1ffc93b9d03e
Valerii Karimov <valeraguess@gmail.com>
2020-09-23 01:31:34 +0300

    Get data from server and upgrade card func

--- js/script.js
+++ js/script.js
@@ -178,32 +178,22 @@ class MenuCard {
   }
 }
 
-new MenuCard(
-  "img/tabs/vegy.jpg",
-  "vegy",
-  'Меню "Фитнес"',
-  'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-  9,
-  '.menu .container'
-).render();
+const getResourse = async (url) => {
+  const res = await fetch(url);
 
-new MenuCard(
-  "img/tabs/elite.jpg",
-  "elite",
-  'Меню “Премиум”',
-  'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-  20,
-  '.menu .container'
-).render();
+  if(!res.ok) {
+    throw new Error(`could not fetch ${url}, status: ${res.status}`);
+  }
 
-new MenuCard(
-  "img/tabs/post.jpg",
-  "post",
-  'Меню "Постное"',
-  'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-  16,
-  '.menu .container'
-).render();
+  return await res.json();
+}
+
+getResourse('http://localhost:3000/menu')
+  .then(data => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    });
+  });
 
 // Forms
 
@@ -216,10 +206,24 @@ const message = {
 }
 
 forms.forEach(item => {
-  postData(item);
+  bindPostData(item);
 });
 
-function postData(form) {
+const postData = async (url, data) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: data    
+  });
+
+  return await res.json();
+}
+
+
+
+function bindPostData(form) {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -233,18 +237,9 @@ function postData(form) {
 
     const formData = new FormData(form);
 
-    const object = {};
-    formData.forEach(function(value, key) {
-      object[key] = value;
-    });
+    const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-    fetch('server.php', {
-      method: "POST",
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(object)
-    }).then(data => data.text())
+    postData('http://localhost:3000/requests', json)
       .then(data => {
           console.log(data);
           showThanksModal(message.success);
@@ -280,4 +275,3 @@ function showThanksModal(message) {
     closeModal();
   }, 4000);
 }
-
