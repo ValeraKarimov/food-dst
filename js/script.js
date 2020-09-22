17988840bd2bcc2a598dcab3d7836e1c1d985baa
Valerii Karimov <valeraguess@gmail.com>
2020-09-23 01:49:34 +0300

    add axios lib

--- js/script.js
+++ js/script.js
@@ -188,9 +188,16 @@ const getResourse = async (url) => {
   return await res.json();
 }
 
-getResourse('http://localhost:3000/menu')
+// getResourse('http://localhost:3000/menu')
+//   .then(data => {
+//     data.forEach(({ img, altimg, title, descr, price }) => {
+//       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+//     });
+//   });
+
+axios.get('http://localhost:3000/menu')
   .then(data => {
-    data.forEach(({ img, altimg, title, descr, price }) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
     });
   });
