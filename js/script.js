3b73e16c2e5dce9f686fd47adc5bf28d85fa5fd7
Valerii Karimov <valeraguess@gmail.com>
2020-09-23 02:30:39 +0300

    add simple slider

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ tabsParent.addEventListener('click', (e) => {
 
 // Timer
 
-const deadline = '2020-09-21';
+const deadline = '2020-12-21';
 
 function getTimeRemaining(endtime) {
   const t = Date.parse(endtime) - Date.parse(new Date());
@@ -282,3 +282,56 @@ function showThanksModal(message) {
     closeModal();
   }, 4000);
 }
+
+// Slider
+
+const slides = document.querySelectorAll('.offer__slide');
+const prev = document.querySelector('.offer__slider-prev');
+const next = document.querySelector('.offer__slider-next');
+const total = document.querySelector('#total');
+current = document.querySelector('#current');
+
+let slideIndex = 1;
+
+
+showSlides(slideIndex);
+
+if(slides.length < 10) {
+  total.textContent = `0${slides.length}`;
+} else {
+  total.textContent = slides.length;
+}
+
+function showSlides(n) {
+  if(n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if(n < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach(item => {
+    item.style.display = 'none';
+  });
+
+  slides[slideIndex - 1].style.display = 'block';
+
+  if(slides.length < 10) {
+    current.textContent = `0${slideIndex}`;
+  } else {
+    current.textContent = slideIndex;
+  }
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+prev.addEventListener('click', () => {
+  plusSlides(-1);
+});
+
+next.addEventListener('click', () => {
+  plusSlides(1);
+});
